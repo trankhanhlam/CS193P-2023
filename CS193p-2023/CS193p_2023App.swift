4f9875cd30373903815103a6d875d8f49e5564f4
trankhanhlam <45313789+trankhanhlam@users.noreply.github.com>
2023-11-16 16:22:25 +0700

    Initial Commit

---
+++ CS193p-2023/CS193p_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  CS193p_2023App.swift
+//  CS193p-2023
+//
+//  Created by Tran Lam on 16/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CS193p_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
