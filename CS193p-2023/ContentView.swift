c3e609d19958ebf5723a351d15d18e1884ccb4c4
trankhanhlam <45313789+trankhanhlam@users.noreply.github.com>
2023-11-17 13:58:30 +0700

    Xong khi xong ở vid 2: https://www.youtube.com/watch?v=sXiD-2XrkKQ&list=PLgRv11u2lkbDo8D5E5SyA408gLd9Ho3cq&index=2&ab_channel=StanfordCS193p

--- CS193p-2023/ContentView.swift
+++ CS193p-2023/ContentView.swift
@@ -8,15 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["👻", "🎃", "🕷️", "😈"]
+    let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙‍♀️", "🙀", "👺", "😱", "☠️", "🍭"]
+    @State var cardCount = 4
     var body: some View {
         // Note: Không thể dùng for trong các Stack. Đọc bên dưới để biết trong ViewBuilder support những loại nào
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content: emojis[index], isFaceUp: true)
+        VStack {
+            ScrollView {
+                cards
             }
+            Spacer()
+            cardCountAdjuster
         }
         .padding()
+    }
+
+    var cards: some View {
+        // minimum size của grid item sẽ là 120
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index], isFaceUp: true)
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+
+    var cardCountAdjuster: some View  {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle) // Tại sao lại set font ở đây. Do Image (dùng của system name) -> Hệ thống sẽ scale image tới cái font được set -> Để image được gần bằng với font chữ. -> Font chữ to -> Ảnh system to
+    }
+
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .imageScale(.large)
+        .font(.largeTitle) // Tại sao lại set font ở đây. Do Image (dùng của system name) -> Hệ thống sẽ scale image tới cái font được set -> Để image được gần bằng với font chữ. -> Font chữ to -> Ảnh system to
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+
+    var cardRemover: some View  {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.fill.badge.plus")
     }
 }
 
@@ -33,30 +76,27 @@ struct CardView: View {
 
      Tạo ra biến với @State là tạo ra 1 tham chiếu tới 1 giá trị nào đó. Nên về lý thuyết khi thay đổi như code ở dòng 53 thì đang thay đổi nơi nó tham chiếu tới (chỗ memory gì đó) chứ không thay đổi pointer
      */
-
     var body: some View {
+        // ở đây không phải là ViewBuilder (bên trong body: với some View không phải là Viewbuilder)
+        // trong này chỉ là function bình thường
         ZStack {
             // ở đây là 1 ViewBuilder
             let base = RoundedRectangle(cornerRadius: 12) // ViewBuilder chỉ có thể support cho 3 thứ
             // 1 - là if else || switch case
             // 2 - là list
             // 3 - là variable assignment -> gắn cho 1 biến 1 cụ thể như line 28
-
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                base.fill(.orange)
-            }
+            }// Dùng Group ở đây vì để thay đổi opacity -> tránh việc khi dùng cardview ở lazyVGrdi (ở vGrid nó sẽ chỉ dùng space thấp nhất cần phải dùng. Sẽ group ở đây và thay đổi opacity thôi thì sẽ keep được chiều cao của cardview là cố định)
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
-//            isFaceUp = !isFaceUp
+            //            isFaceUp = !isFaceUp
             isFaceUp.toggle() //toggle thuộc extension của Boolen thôi nhá
         }
-
-
-
         .foregroundColor(.orange)
         .imageScale(.small)
     }
